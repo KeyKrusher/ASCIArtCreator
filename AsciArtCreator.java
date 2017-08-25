f8750cbf313f64adef0871f612af19f596b61039
Jack "Chubs" Clark <clark.jack554@gmail.com>
2017-08-25 09:13:12 -0500

    Backup before I scrap everything
    I don't think that i need half of the stuff that is in the program, so I'll probably get rid of most of this program.

--- AsciArtCreator.java
+++ AsciArtCreator.java
@@ -1,4 +1,8 @@
 import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.FileReader;
 
 // Code written by Jackson "Chubs" Clark
 // This code turns a string into ascii art that works!
@@ -11,19 +15,26 @@ import java.util.Scanner;
 // Make it support multiple programming languages
 // Make help function for both command line and 
 
-public class AsciArtCreator {
+// https://www.mkyong.com/java/how-to-read-file-from-java-bufferedreader-example/
+// fw = new FileWriter(FILENAME);
+// bw = new BufferedWriter(fw);
+
+public class AsciArtCreator
+{
+	Scanner input = new Scanner(System.in);
+	
 	public String asciStringCreator(String input)
 	{
 		for (int i = 0; i < input.length(); i++)
 		{
-			// Replace
+			// Replace every backslash with 2 backslashes, or just add another backslash right after
 		}
 		
 		return input;
 	}
 	public void writeStringToFile(String outputString, String filename)
 	{
-		// Write stream to file
+		// Write string to file
 	}
 	public void writeFileTofile(/* Get each file name and path and such */)
 	{
@@ -36,14 +47,31 @@ public class AsciArtCreator {
 	public static void intro()
 	{
 		// Output tutorial type thing
+		System.out.println("Welcome to the AsciArt creator for Java!");
+		System.out.println("To use, either give input as directed, or type 'options' to see what you can do.");
+		System.out.println("If you would like to see a help menu, type 'help' now:")
+		doesUserNeedHelp = input.next().toLowerCase();
+		helpIsNeeded = doesUserNeedHelp == "help" ? help();
+		
 	}
-	public static void main(String[] args) {
+	public void options(String option)
+	{
+		
+	}
+	public boolean test()
+	{
+		// Give the program test data, and check results then return it as a boolen
+		// Yes, I'm not writing a seperate unit test, deal with it.
+	}
+	public static void main(String[] args)
+	{
 		// Get input from either the command line arguments or the normal console
 		intro();
 		// Check for command line arguments, if none, use Scanner input, else, use command line arguments
 		if (args.length != 0)
 		{
 			// Run command line arguments
+			args[0] == "help" ? help();
 		}
 		else
 		{
